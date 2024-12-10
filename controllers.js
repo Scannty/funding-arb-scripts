8a462c4494d0b2c56b20a5f7e65e0af2a2eba0c0
Aleksa Uroseivc <aleksa.urosevic0@gmail.com>
2024-12-10 18:08:13 +0100

    feat: Add methods for retrieving specific perp data

--- controllers.js
+++ controllers.js
@@ -1,4 +1,7 @@
-async function getHyperliquidData(req, res) {
+const fs = require("fs");
+require("dotenv").config();
+
+async function getTopVolumePerps(req, res) {
   console.log("Getting data...");
   const response = await fetch("https://api.hyperliquid.xyz/info", {
     method: "POST",
@@ -28,8 +31,6 @@ async function getHyperliquidData(req, res) {
       continue;
     }
 
-    console.log(item);
-
     const response = await fetch("https://api.hyperliquid.xyz/info", {
       method: "POST",
       headers: {
@@ -38,7 +39,7 @@ async function getHyperliquidData(req, res) {
       body: JSON.stringify({
         type: "fundingHistory",
         coin: item.name,
-        startTime: Date.now() - 365 * 24 * 60 * 60 * 1000, // past year
+        startTime: Date.now() - 20 * 24 * 3600 * 1000, // past year
       }),
     });
     const fundingData = await response.json();
@@ -83,4 +84,91 @@ async function getCurrentMidPrice(req, res) {
   });
 }
 
-module.exports = { getHyperliquidData, getCurrentMidPrice };
+async function getPerpsInfo(req, res) {
+  const perps = req.query.perps ? req.query.perps.split(",") : [];
+
+  console.log("Getting data...");
+  const response = await fetch("https://api.hyperliquid.xyz/info", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ type: "metaAndAssetCtxs" }),
+  });
+
+  const data = await response.json();
+  const perpsInfo = [];
+
+  for (const [index, item] of data[0]["universe"].entries()) {
+    if (!perps.includes(item.name)) {
+      continue;
+    }
+    if (perps.length === perpsInfo.length) {
+      break;
+    }
+
+    const response = await fetch("https://api.hyperliquid.xyz/info", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        type: "fundingHistory",
+        coin: item.name,
+        startTime: Date.now() - 20 * 24 * 3600 * 1000, // past 20 days, currently max
+      }),
+    });
+    const fundingData = await response.json();
+
+    let totalFunding = 0;
+    fundingData.forEach((fundingItem) => {
+      totalFunding += Number(fundingItem.fundingRate);
+    });
+    totalFunding = totalFunding * 100;
+    const avgFundingHrly = totalFunding / fundingData.length;
+    const avgFundingYrly = avgFundingHrly * 24 * 365;
+
+    perpsInfo.push({
+      name: item.name,
+      assetIndex: index,
+      decimals: item.szDecimals,
+      fundingHrly: Number(data[1][index]["funding"] * 100).toFixed(4),
+      fundingYrly: Number(data[1][index]["funding"] * 100 * 24 * 365).toFixed(
+        4
+      ),
+      fundingAvgMonthly: Number(avgFundingYrly).toFixed(4),
+    });
+  }
+
+  res.status(200).json(perpsInfo);
+}
+
+function storeTradeInfo(req, res) {
+  const { userAddress, tradeInfo } = req.body;
+  console.log(tradeInfo);
+
+  if (!fs.existsSync("tradeInfo.json")) {
+    fs.writeFileSync(
+      "tradeInfo.json",
+      JSON.stringify([
+        {
+          userAddress,
+          tradeInfo,
+        },
+      ])
+    );
+  } else {
+    const tradeInfoArray = JSON.parse(fs.readFileSync("tradeInfo.json"));
+    tradeInfoArray.push(tradeInfo);
+    fs.writeFileSync("tradeInfo.json", JSON.stringify(tradeInfoArray));
+  }
+
+  res.status(200).json({ message: "Trade info stored successfully!" });
+}
+
+module.exports = {
+  getTopVolumePerps,
+  getCurrentMidPrice,
+  getPerpsInfo,
+  storeTradeInfo,
+};
--- server.js
+++ server.js
@@ -4,10 +4,13 @@ const controllers = require("./controllers");
 
 const app = express();
 
+app.use(express.json());
 app.use(cors());
 
-app.get("/getHyperliquidData", controllers.getHyperliquidData);
+app.get("/getTopVolumePerps", controllers.getTopVolumePerps);
 app.get("/getCurrentMidPrice", controllers.getCurrentMidPrice);
+app.get("/getPerpsInfo", controllers.getPerpsInfo);
+app.post("/storeTradeData", controllers.storeTradeInfo);
 
 app.listen(8000, () => {
   console.log("Server is listening on port 8000");
