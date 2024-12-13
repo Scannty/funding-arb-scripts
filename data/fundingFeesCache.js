3dcd379b8aa7d176ce19bab9141f88bde19d08e6
Skocimis <aki.spremo@gmail.com>
2024-12-13 18:44:46 +0100

    feat+refactor: HYPE and PURR

--- data/fundingFeesCache.js
+++ data/fundingFeesCache.js
@@ -1,26 +1,4 @@
-const tokens = {
-    ETH: {
-        tokenAddress: "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1",
-        decimals: 18,
-        iconSymbol: "eth",
-    },
-    BTC: {
-        tokenAddress: "0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f",
-        decimals: 8,
-        iconSymbol: "btc",
-    },
-    LINK: {
-        tokenAddress: "0xf97f4df75117a78c1A5a0DBb814Af92458539FB4",
-        decimals: 18,
-        iconSymbol: "link",
-    },
-    ARB: {
-        tokenAddress: "0x912CE59144191C1204E64559FE8253a0e49E6548",
-        decimals: 18,
-        iconSymbol: "arbi",
-    },
-};
-
+const tokens = ['ETH', 'BTC', 'LINK', 'ARB', 'HYPE', 'PURR'];
 const fees = {};
 
 const updateTicker = async (ticker) => {
@@ -45,7 +23,7 @@ const loadTicker = async (ticker) => {
 }
 
 const update = async (init) => {
-    for (const ticker in tokens) {
+    for (const ticker of tokens) {
         if (init)
             fees[ticker] = await loadTicker(ticker);
         let last_timestamp = fees[ticker][fees[ticker].length - 1]?.time || 1702392469448;
